8a79744b5b78db2721a333bffdde7befcba285f9
wasfafasd <124546451+wasfafasd@users.noreply.github.com>
2025-04-14 13:27:43 -0400

    Update Library1.lua

--- Library1.lua
+++ Library1.lua
@@ -3984,7 +3984,7 @@ function Library:CreateWindow(WindowInfo)
         Library.KeybindFrame, Library.KeybindContainer = Library:AddDraggableMenu("Keybinds")
         Library.KeybindFrame.AnchorPoint = Vector2.new(0, 0.5)
         Library.KeybindFrame.Position = UDim2.new(0, 6, 0.5, 0)
-        Library.KeybindFrame.Visible = false
+        Library.KeybindFrame.Visible = true
         Library:UpdateDPI(Library.KeybindFrame, {
             Position = false,
             Size = false,
