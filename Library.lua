f95f5029c096fe9fb8b27cb2d914a76e56b85676
wasfafasd <124546451+wasfafasd@users.noreply.github.com>
2025-04-14 12:22:29 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -66,7 +66,7 @@ local Library = {
     Signals = {},
     UnloadSignals = {},
 
-    MinSize = Vector2.new(480, 360),
+    MinSize = Vector2.new(720, 360),
     DPIScale = 1,
     CornerRadius = 4,
 
@@ -74,7 +74,7 @@ local Library = {
     Scheme = {
         BackgroundColor = Color3.fromRGB(15, 15, 15),
         MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
+        AccentColor = Color3.fromRGB(255, 255, 255),
         OutlineColor = Color3.fromRGB(40, 40, 40),
         FontColor = Color3.new(1, 1, 1),
         Font = Font.fromEnum(Enum.Font.Code),
