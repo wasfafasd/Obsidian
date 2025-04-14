b4b0f2db7ad06f5fad96d04cf6068087560c9307
wasfafasd <124546451+wasfafasd@users.noreply.github.com>
2025-04-14 13:12:03 -0400

    Update Library1.lua

--- Library.lua
+++ Library.lua
@@ -5014,7 +5014,6 @@ function Library:CreateWindow(WindowInfo)
         end
 
         MainFrame.Visible = Library.Toggled
-        ModalElement.Modal = Library.Toggled
 
         if Library.Toggled and not Library.IsMobile then
             local OldMouseIconEnabled = UserInputService.MouseIconEnabled
